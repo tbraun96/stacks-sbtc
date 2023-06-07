f949e25a83961ae9580901e1af0116731a6d1756
Stjepan Golemac <stjepan.golemac@gmail.com>
2023-06-07 14:22:21 +0200

    feat: Make relay port configurable (#414)
    * feat: Make relay port configurable
    
    * fix: apply suggestions

--- relay-server/src/bin/relay-server.rs
+++ relay-server/src/bin/relay-server.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
 use relay_server::Server;
 
 fn main() {
-    let addr = "127.0.0.1:9776";
-    Server::run(addr);
+    let args = Cli::parse();
+    Server::run(&args.listen);
+}
+
+#[derive(Parser)]
+struct Cli {
+    /// Where to listen for incoming connections
+    #[clap(short, long, default_value = "127.0.0.1:9776")]
+    listen: String,
 }
